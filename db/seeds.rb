b34444c656572bf99257a4e74f0ee70033869fc0
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-08-22 16:59:28 +0400

    create seeds for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "cleaning DB"
+User.destroy_all
+Topic.destroy_all
+
+puts "Creating users ..."
+
+users = [
+  { first_name: 'John', last_name: 'Doe', email: 'john@gmail.com', password: '123456' },
+  { first_name: 'Jane', last_name: 'Doe', email: 'jane@gmail.com', password: '123456' },
+  { first_name: 'Jon', last_name: 'Snow', email: 'jon@gmail.com', password: '123456' },
+  { first_name: 'Jack', last_name: 'Jones', email: 'jack@gmail.com', password: '123456' }
+  # {first_name: 'Joe', last_name: 'Blogs', email: 'joe@gmail.com', password: '123456'},
+  # {first_name: 'David', last_name: 'Smith', email: 'david@gmail.com', password: '123456'},
+  # {first_name: 'Jenny', last_name: 'Smith', email: 'jenny@gmail.com', password: '123456'},
+  # {first_name: 'Jamey', last_name: 'Johnson', email: 'jamey@gmail.com', password: '123456'}
+]
+
+users.each do |attributes|
+  User.create!(attributes)
+end
+
+puts "Finished creating users!"
+
+
 puts "Creating topic titles..."
 
 Topic.create(title: "bitcoin")
@@ -13,4 +37,4 @@ Topic.create(title: "ethereum")
 Topic.create(title: "nft")
 Topic.create(title: "defi")
 
-puts "Finished!"
+puts "Finished creating topics!"
