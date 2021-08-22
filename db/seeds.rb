457e89c5553268ca828ed6deea13993c5c952e5b
Khamza Habibullah <habibullahkhamza@gmail.com>
2021-08-22 16:14:44 +0400

    created seed for topic model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating topic titles..."
+
+Topic.create(title: "Good article")
+Topic.create(title: "Bad article")
+Topic.create(title: "NFT article")
+Topic.create(title: "Bitcoin article")
+
+puts "Finished!"
