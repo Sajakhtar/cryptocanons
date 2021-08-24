7add0df615bcda81bc67eae3a0ee2070a160665e
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-08-24 13:48:56 +0400

    remove topic model's validation on title for inclusion

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,5 @@
 class Topic < ApplicationRecord
   has_many :favorite_topics
-  CRYPTO = ['nft', 'bitcoin', 'defi']
-  validates :title, presence: true, inclusion: { in: CRYPTO }
 
-
+  validates :title, presence: true
 end
