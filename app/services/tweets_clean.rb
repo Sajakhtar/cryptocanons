460f2f7c95ee33a696ff18cc46c5a97cb5102c15
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-08-25 21:15:25 +0400

    refactor sort desc in tweet clean service

--- app/services/tweets_clean.rb
+++ app/services/tweets_clean.rb
@@ -13,19 +13,16 @@ class TweetsClean
     @tweets_data.map! do |tweet|
       author = @tweets_users.find { |user| user['id'] == tweet['author_id'] }
       tweet['user'] = author
-      # if tweet's user has more than 1K followers then do
+      # if tweet's user has more than 1K followers then do ******
       # tweet.delete(:author_id)
         tweet['tweet_url'] = "https://twitter.com/#{tweet['user']['username']}/status/#{tweet['id']}"
         tweet
       # else
-        # remove tweet from array
-        # @tweets_data.delete(tweet)
+        # remove tweet from array ******* @tweets_data.delete(tweet)
     end
     @tweets_data.select! { |tweet| tweet['user']["public_metrics"]["followers_count"] > 1000  }
 
-    @tweets_data.sort! { |a,b| b['user']["public_metrics"]["followers_count"] <=> a['user']["public_metrics"]["followers_count"]}
-    # @tweets_data = @tweets_data.sort_by {|tweet| tweet['user']["public_metrics"]["followers_count"] }.reverse
-    # https://twitter.com/#{tweet['user']['username']}/status/#{tweet['id']}
+    @tweets_data = @tweets_data.sort_by {|tweet| tweet['user']["public_metrics"]["followers_count"] }.reverse
 
     @tweets_data = @tweets_data.first(@number_of_tweets)
   end
