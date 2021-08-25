c1c500acd492560d0120dc89c26de0d8e760b89b
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-08-25 08:10:20 +0400

    topic migration to add icon-url, cashtag, symbol, coingecko-id, and  update seeds to create topics from coinmarketcap api and add coingecko icon url

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,5 @@ class Topic < ApplicationRecord
   has_many :favorite_topics
 
   validates :title, presence: true
+  validates :icon_url, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_22_121216) do
+ActiveRecord::Schema.define(version: 2021_08_25_030551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,10 @@ ActiveRecord::Schema.define(version: 2021_08_22_121216) do
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "symbol"
+    t.string "cashtag"
+    t.string "icon_url"
+    t.string "coingecko_id"
   end
 
   create_table "users", force: :cascade do |t|
