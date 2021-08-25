bdb133f7bf402c324d4c6b5746c952a908e76f5e
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-08-25 16:08:29 +0400

    tweet url within private method rahter than view

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -14,11 +14,14 @@ class TopicsController < ApplicationController
       author = @tweets_users.find { |user| user['id'] == tweet['author_id'] }
       tweet['user'] = author
       # tweet.delete(:author_id)
+      tweet['tweet_url'] = "https://twitter.com/#{tweet['user']['username']}/status/#{tweet['id']}"
       tweet
     end
     @tweets_data.select! { |tweet| tweet['user']["public_metrics"]["followers_count"] > 1000  }
 
     @tweets_data.sort! { |a,b| b['user']["public_metrics"]["followers_count"] <=> a['user']["public_metrics"]["followers_count"]}
+
+    # https://twitter.com/#{tweet['user']['username']}/status/#{tweet['id']}
 
     @tweets_data = @tweets_data.first(5)
   end
