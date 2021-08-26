b2500a14087aee8da6d147b6465219bdbb83eb5d
Khamza Habibullah <43613044+khamzahabibullah@users.noreply.github.com>
2021-08-26 15:43:28 +0400

    Homepage design (#41)
    * design homepage
    
    * design homepage
    
    * design home
    
    * bugfix select form partial topic_id
    
    * homepage join now redirect to sign up
    
    * remove aos js script tags from applicatioin html
    
    * refactor aos js by moving into a js module then import into application js
    
    * remove commented out aos code from app js
    
    Co-authored-by: Sajid Akhtar <sajid.i.akhtar@gmail.com>

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,15 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
-
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
 
 // ----------------------------------------------------
 // Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
@@ -23,8 +22,10 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { initAos } from 'plugins/init_aos';
 
-document.addEventListener('turbolinks:load', () => {
+document.addEventListener("turbolinks:load", () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initAos();
 });
