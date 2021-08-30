7ce3427c3d66722f4d4a70b54831d59e1895dfc1
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-08-30 14:44:21 +0400

    topics controller search action and search route

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,15 +2,16 @@ class TopicsController < ApplicationController
   skip_before_action :authenticate_user!, only: :show
 
   def show
-    if params[:search]
-      @topic = Topic.find(params[:search][:topic])
-    else
-      @topic = Topic.find(params[:id])
-    end
+    @topic = Topic.find(params[:id])
 
-    @topics = Topic.all
     # raise
     @tweets_data = HandleTweets.new(@topic, 5).format_tweets
     @is_favorite = !FavoriteTopic.where(topic: @topic, user: current_user).empty?
+  end
+
+  def search
+    @topic = Topic.find(params[:search][:topic])
+    # raise
+    redirect_to @topic
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
+import "controllers"
 
 Rails.start();
 Turbolinks.start();
@@ -19,24 +20,24 @@ ActiveStorage.start();
 
 // External imports
 import "bootstrap";
-import "select2";
-import "select2/dist/css/select2.css";
-import $ from "jquery";
+// import "select2";
+// import "select2/dist/css/select2.css";
+// import $ from "jquery";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { initAos } from "plugins/init_aos";
 import { initPrice } from "plugins/init_price";
-import { initChartkick } from "plugins/init_chartkick";
+// import { initChartkick } from "plugins/init_chartkick";
+import { initSelect } from "plugins/init_select2";
 
 document.addEventListener("turbolinks:load", () => {
   // Call your functions here, e.g:
   // initSelect2();
   initAos();
   // initChartkick();
-  $("#search_topic").select2({
-    placeholder: "Select a topic",
-  });
-  initPrice();
+  // $("#search_topic").select2({
+  //   placeholder: "Select a topic",
+  // });
+  initSelect()
+  initPrice()
 });
-
-import "controllers"
