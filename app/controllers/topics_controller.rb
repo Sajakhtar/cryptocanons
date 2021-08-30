7d7e0b79621a511a7acc750a23174ac95c128b4b
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-08-30 14:24:30 +0400

    topic show adjust @topics if there are search params with topic id

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,11 +2,14 @@ class TopicsController < ApplicationController
   skip_before_action :authenticate_user!, only: :show
 
   def show
-    # if params[search] take params[:search][:id]
-    #else take params[:id]
-    @topic = Topic.find(params[:id])
+    if params[:search]
+      @topic = Topic.find(params[:search][:topic])
+    else
+      @topic = Topic.find(params[:id])
+    end
+
     @topics = Topic.all
-    raise
+    # raise
     @tweets_data = HandleTweets.new(@topic, 5).format_tweets
     @is_favorite = !FavoriteTopic.where(topic: @topic, user: current_user).empty?
   end
