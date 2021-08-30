ccbd0d2430aa3f1fa452e9619fc7b1c37292cac9
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-08-30 12:36:11 +0400

    search off cavans preliminary work down

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,7 +2,11 @@ class TopicsController < ApplicationController
   skip_before_action :authenticate_user!, only: :show
 
   def show
+    # if params[search] take params[:search][:id]
+    #else take params[:id]
     @topic = Topic.find(params[:id])
+    @topics = Topic.all
+    raise
     @tweets_data = HandleTweets.new(@topic, 5).format_tweets
     @is_favorite = !FavoriteTopic.where(topic: @topic, user: current_user).empty?
   end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,11 +32,11 @@ document.addEventListener("turbolinks:load", () => {
   // Call your functions here, e.g:
   // initSelect2();
   initAos();
-  initPrice();
-  initChartkick();
+  // initChartkick();
   $("#search_topic").select2({
     placeholder: "Select a topic",
   });
+  initPrice();
 });
 
 import "controllers"
