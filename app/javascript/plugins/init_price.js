f6a340ef2a43e431696fcd6228bbe647f0810482
Khamza Habibullah <habibullahkhamza@gmail.com>
2021-09-01 10:58:10 +0400

    design updates

--- app/javascript/plugins/init_price.js
+++ app/javascript/plugins/init_price.js
@@ -1,36 +1,42 @@
 const fetchPrice = (priceElements) => {
   priceElements.forEach((priceEl) => {
-
-    fetch(`https://api.coingecko.com/api/v3/coins/${priceEl.dataset.coingeckoId}`)
-      .then(response => response.json())
+    fetch(
+      `https://api.coingecko.com/api/v3/coins/${priceEl.dataset.coingeckoId}`
+    )
+      .then((response) => response.json())
       .then((data) => {
-        const price = data['market_data']['current_price']['usd'];
-        const priceFormatted = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(price);
+        const price = data["market_data"]["current_price"]["usd"];
+        const priceFormatted = new Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD",
+        }).format(price);
 
-        const change24hr = data['market_data']['price_change_percentage_24h'];
-        const change24hrFormatted = change24hr.toFixed(1)
+        const change24hr = data["market_data"]["price_change_percentage_24h"];
+        const change24hrFormatted = change24hr.toFixed(1);
 
-        priceEl.innerText = priceFormatted
-
-        const change24hrEl = priceEl.nextElementSibling
-        change24hrEl.innerText = `${change24hrFormatted}% 24h`
+        priceEl.innerText = priceFormatted;
+        const iconDownArrow = `<i class="fas fa-arrow-down"></i>`;
+        const iconUpArrow = `<i class="fas fa-arrow-up"></i>`;
+        const icon = change24hr > 0 ? iconUpArrow : iconDownArrow;
+        const change24hrEl = priceEl.nextElementSibling;
+        change24hrEl.innerHTML = `${icon} ${change24hrFormatted}% 24h`;
 
         if (change24hr > 0) {
-          change24hrEl.classList.add('text-success')
+          change24hrEl.classList.add("text-success");
         } else {
-          change24hrEl.classList.add('text-danger')
+          change24hrEl.classList.add("text-danger");
         }
       });
   });
-}
+};
 
 const initPrice = () => {
-  const priceElements = document.querySelectorAll('.price')
+  const priceElements = document.querySelectorAll(".price");
 
-  if(priceElements.length) {
-    fetchPrice(priceElements)
+  if (priceElements.length) {
+    fetchPrice(priceElements);
     setInterval(fetchPrice, 60000, priceElements);
   }
-}
+};
 
-export { initPrice }
+export { initPrice };
